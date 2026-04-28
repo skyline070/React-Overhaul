6784a75f217290510821722ad66204ade1df95b8
skyline070 <yadavsumitkumar9724@gmail.com>
2026-04-28 22:10:57 +0530

    r7.3

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -22,6 +22,9 @@ const RestaurantMenu = () => {
     };
     if (resInfo === null) return <Shimmer />
 
+    // console.log(resInfo);
+
+
     // 🔹 Loop through all cards to find the one that contains restaurant info
     // API response structure is dynamic, so we avoid hardcoding index like [2]
     const restaurantCard = resInfo?.cards?.find(
@@ -34,21 +37,47 @@ const RestaurantMenu = () => {
     const { name, cuisines, costForTwoMessage } =
     restaurantCard?.card?.card?.info || {};
 
-    const { itemCards } = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card;
+    // const { itemCards } = resInfo?.cards[5]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card;
+    // console.log(itemCards);
+    // 🔹 Get ALL menu sections (categories)
+    const menuSections =
+    resInfo?.cards
+        ?.find((c) => c?.groupedCard)
+        ?.groupedCard?.cardGroupMap?.REGULAR?.cards || [];
 
-    console.log(itemCards);
+    // 🔹 Extract all items from all categories
+    const itemCards = menuSections
+    .map((section) => section?.card?.card?.itemCards)
+    .filter(Boolean) // remove undefined
+    .flat(); // merge all arrays
+
+    console.log(menuSections);
+
+    // 🔥 🔹 REMOVE DUPLICATES (IMPORTANT)
+    const uniqueItemCards = itemCards.filter(
+    (item, index, self) =>
+        index ===
+        self.findIndex(
+        (t) => t?.card?.info?.id === item?.card?.info?.id
+        )
+    );
+
+    // ✅ Use this in UI
+    console.log(uniqueItemCards);
+
 
         
     return (
         <div>
             <h1>{name}</h1>
-            <p>{cuisines.join(",")} - {costForTwoMessage}</p>
+            <p>{cuisines.join(", ")} - {costForTwoMessage}</p>
             <h2>Menu</h2>
             <ul>
-                {itemCards.map((item) => 
-                <li key={item.card.info.id}>
-                    {item.card.info.name} - {"Rs."}
-                    {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                {uniqueItemCards.map((item) => 
+                <li key={item?.card?.info?.id}>
+                   <h3> {item?.card?.info?.name}</h3> - {"Rs."}
+                    {item?.card?.info?.price / 100 || item?.card?.info?.defaultPrice / 100}
+                    <h5>{item?.card?.info?.description}</h5>
                 </li>)}
             
             </ul>
