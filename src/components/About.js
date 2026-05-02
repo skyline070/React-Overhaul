127d9abd4de5c93b9cd4c499edad97bba27ad48e
skyline070 <yadavsumitkumar9724@gmail.com>
2026-05-03 01:19:13 +0530

    r8

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,32 @@
-const About = () => {
-    return(
+import User from "./User";
+import UserClass from "./UserClass";
+import {Component} from "react";
+
+class About extends Component {
+    constructor(props){
+        super(props);
+
+    // console.log("Parent Constuctor");
+    };
+
+    componentDidMount(){
+        // console.log("Parent CDM")
+    };
+
+    render(){
+
+        // console.log("Parent Render")
+         return(
         <div>
-            <h1>About US</h1>
+            <h1>About US </h1>
+            <h2>Developed By :-</h2>
+
+            <UserClass name={"first"} location={"bly"}/>
+            
         </div>
-    );
+        );
+    };
 };
+
 
 export default About;
