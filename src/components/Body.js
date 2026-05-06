23786c8a61ac2d0cc49dd512658b65b7670833a2
skyline070 <yadavsumitkumar9724@gmail.com>
2026-05-06 16:47:39 +0530

    r9

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
   // 🔹 State
@@ -88,6 +89,15 @@ const Body = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, [loading]);
+
+  const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false)
+      return (
+    <h1>
+      Looks like you're offline!! Please check your internet connection;
+    </h1>
+    );
 
   // 🔹 Loading UI
   if (listOfRestaurants.length === 0) {
